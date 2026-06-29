0d54ce938a8922a24457ff842d52b2aafe343f9e
Aachal Arvind Singh <136174448+Aachal107@users.noreply.github.com>
2026-06-29 11:07:43 +0000

    variables and datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let email="abc@gmail.com"
+var password="852369"
+city="jaipur"
+
+email="aachal@gmail.com"
+password="2589640"
+city="surat"
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,email,password,city])
