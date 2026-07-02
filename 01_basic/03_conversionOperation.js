64a0ae1aff04a196630fd2af7820e404b94d9ed8
Aachal Arvind Singh <136174448+Aachal107@users.noreply.github.com>
2026-07-02 18:22:30 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "33"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
